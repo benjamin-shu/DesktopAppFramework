db57341ce53bd60928824818ca913095b50945f6
bensh_000 <bensh_000@192.168.1.4>
2018-01-04 17:38:35 -0500

    Changed GitHub username - testing redirect for local repository.

--- DesktopAppFramework/src/daf/ui/AppGUI.java
+++ DesktopAppFramework/src/daf/ui/AppGUI.java
@@ -1,5 +1,6 @@
 package daf.ui;
 
+import daf.AppTemplate;
 import daf.file.*;
 
 import javafx.application.Application;
@@ -14,10 +15,29 @@ import javafx.scene.image.Image;
  * @author Benjamin Shu
  */
 public abstract class AppGUI {
+    /**
+     * The AppTemplate instance that this AppGUI is associated with.
+     */
+    protected AppTemplate app;
+    /**
+     * The application's desktop and task bar icon.
+     */
     protected Image icon;
+    /**
+     * The application's name/title.
+     */
     protected String title;
+    /**
+     * 
+     */
     protected Stage stage;
+    /**
+     * 
+     */
     protected Scene scene;
+    /**
+     * 
+     */
     protected Pane layout;
 }
 class EditorGUI extends AppGUI {
